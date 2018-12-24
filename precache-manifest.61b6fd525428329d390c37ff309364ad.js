c8fffa8b43ac33b54bd3aa51ec6e94c2c27f91a7
tombonan <tombonan018@gmail.com>
2018-12-24 00:27:45 -0700

    Updates

---
+++ precache-manifest.61b6fd525428329d390c37ff309364ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6194906144c2160af8",
+    "url": "/emoji/static/js/main.7b619490.chunk.js"
+  },
+  {
+    "revision": "f0a6f2a0708a753e6abc",
+    "url": "/emoji/static/js/1.f0a6f2a0.chunk.js"
+  },
+  {
+    "revision": "e1be33dba687f3227822",
+    "url": "/emoji/static/js/runtime~main.e1be33db.js"
+  },
+  {
+    "revision": "2926fcfaa9593825e603c6e6a5deeb64",
+    "url": "/emoji/index.html"
+  }
+];
